86a23334147bb9c51aa2eb7437ab2e0767395801
Miguel Angel Herrojo <rmiguelangel222@gmail.com>
2025-11-21 00:23:11 +0100

    Creacion de array con nombres de jugadores y modificacion de estilos del inventario

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ import { Jugador } from './modules/jugador.js';
 import { categorizarJugadores, rankearJugadores } from './modules/ranking.js';
 import { Enemigo, JefeFinal } from './modules/Enemigo.js';
 
+let jugadores = ['Cazador','Cazadora','Asesino','Guardian','Caballero'];
 
-const jugador = new Jugador('Ejemplo');
+
+const jugador = new Jugador(jugadores[1]);
 const enemigo = new Enemigo('Enemigo', 'Goblin', 3, 30);
 
 console.log(jugador.atacar(enemigo));
