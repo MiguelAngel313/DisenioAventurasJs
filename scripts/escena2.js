df499b0eb5e3c0e6249dcfa33b85647c65fb8d94
Miguel Angel Herrojo <rmiguelangel222@gmail.com>
2025-11-25 21:02:58 +0100

    Animacion del boton comprar añadida

--- scripts/escena2.js
+++ scripts/escena2.js
@@ -1,9 +1,13 @@
 import { Producto } from '../modules/Producto.js';
 let precios = document.querySelectorAll('.precio');
 let productos = crearProducto();
+const botonComprar = document.querySelectorAll('.comprar');
 
-for (let i = 0; i < precios.length; ++i) {
-    precios[i].textContent = productos[i].precio + '€';    
+establecerPrecios();
+function establecerPrecios(){
+    for (let i = 0; i < precios.length; ++i) {
+        precios[i].textContent = productos[i].precio + '💶';    
+    }
 }
 
 
@@ -15,12 +19,27 @@ function crearProducto(){
     const anillo = new Producto('Anillo', 200, 'Comun', 'Salud', 10);
     const martillo = new Producto('Martillo', 300, 'Comun', 'Ataque', 10);
     const pocion = new Producto('Pocion', 100, 'Comun', 'Salud', 10);
-    const hoja = new Producto('Hoja', 100, 'Comun', 'Ataque', 10);
+    const hoja = new Producto('Hoja', 200, 'Comun', 'Ataque', 10);
     const arrayProductos = [casco, armadura, anillo, martillo, pocion, hoja];
     
     return arrayProductos;
 }
 
 
-
-
+    botonComprar.forEach(boton => {
+        boton.addEventListener('click', function() {
+            let imagen = this.parentElement.querySelector('img');
+            if (this.textContent === 'Comprar') {
+                this.textContent = 'Vender';
+                this.style.backgroundColor = 'red';
+                this.parentElement.querySelector('.precio').textContent = '0💶';
+                imagen.style.filter = 'grayscale(100%)';
+                
+            } else {
+                this.textContent = 'Comprar';
+                this.style.backgroundColor = 'blanchedalmond';
+                establecerPrecios();
+                imagen.style.filter = 'grayscale(0%)';
+            }
+        });
+    });
