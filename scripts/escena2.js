5cb87e0b76d9791e5e293d65204fc0b17db155ef
Miguel Angel Herrojo <rmiguelangel222@gmail.com>
2025-11-25 20:18:01 +0100

    Solucion de error de importacion de escena2.js solucionados

--- scripts/escena2.js
+++ scripts/escena2.js
@@ -1,11 +1,25 @@
-// TODO: Mirar import y export con objetos en js
+import { Producto } from '../modules/Producto.js';
+let precios = document.querySelectorAll('.precio');
+let productos = crearProducto();
 
-let productos = document.querySelectorAll('.precio');
+for (let i = 0; i < precios.length; ++i) {
+    precios[i].textContent = productos[i].precio + '€';    
+}
 
 
-productos.forEach(p =>{
-    p.textContent = 400;
-})
+
+//Creacion de todos los productos del mercado
+function crearProducto(){
+    const casco = new Producto('Casco', 400, 'Comun', 'Defensa', 10);
+    const armadura = new Producto('Armadura', 600, 'Comun', 'Defensa', 10);
+    const anillo = new Producto('Anillo', 200, 'Comun', 'Salud', 10);
+    const martillo = new Producto('Martillo', 300, 'Comun', 'Ataque', 10);
+    const pocion = new Producto('Pocion', 100, 'Comun', 'Salud', 10);
+    const hoja = new Producto('Hoja', 100, 'Comun', 'Ataque', 10);
+    const arrayProductos = [casco, armadura, anillo, martillo, pocion, hoja];
+    
+    return arrayProductos;
+}
 
 
 
