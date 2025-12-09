0f008114734ac91000d93fb79a01d2d44916ec6c
Miguel Angel Herrojo <rmiguelangel222@gmail.com>
2025-12-09 12:52:39 +0100

    Solucion de error de muestreo de inventario.

--- scripts/escena5.js
+++ scripts/escena5.js
@@ -3,8 +3,18 @@ const enemigos = JSON.parse(localStorage.getItem('enemigos'));
 const imgInventario = document.querySelectorAll('.inventario img');
 
 enemigos.forEach((enemigo, i) => {
-    console.table(`Enemigo ${i + 1}: `, enemigo);
+    console.log(`Enemigo ${i + 1}: `, enemigo);
 });
+
+mostrarInventario();
+
+function mostrarInventario(){
+    for (let i = 0; i < jugador.inventario.length; i++) { 
+            imgInventario[i].src = jugador.inventario[i].img;
+        }
+    }
+
+
 
 
 
